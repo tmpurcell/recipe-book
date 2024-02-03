4664f63c1c51cfb2a65337235e9a426f25c5aed2
hriewe <hriewe12@gmail.com>
2024-02-03 03:46:38 -0500

    Add basic login func to backend.

--- Backend/main.py
+++ Backend/main.py
@@ -1,16 +1,22 @@
 from typing import Union
 from pydantic import BaseModel
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+class Login(BaseModel):
+    username: str
+    password: str
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+)
+@app.post("/login/")
+async def log_in(login: Login):
+    return login
