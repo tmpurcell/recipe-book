cd9f3fb846aa5028a7634058618f6cc91503cc34
hriewe <hriewe12@gmail.com>
2024-02-03 20:50:31 -0500

    Change steps from list to string.

--- Backend/main.py
+++ Backend/main.py
@@ -33,14 +33,14 @@ async def image_upload(image: Image):
         recipe = {
                     'title': 'Blueberry Muffins',
                     'ingredients': ['2 cups all-purpose flour', '1 cup granulated sugar', '2 teaspoons baking powder', '1/2 teaspoon salt', '1/2 cup melted butter', '1/2 cup milk', '1 teaspoon vanilla extract', '2 cups blueberries'],
-                    'steps': ['Preheat oven to 400 degrees. Prepare a muffin pan. In a large bowl, sift together flour, sugar, baking powder, and salt.', 'In a medium bowl, whisk in eggs. Add butter, milk and vanilla. Add to flour mixture and stir until combined. Fold in blueberries.', 'Add mix to muffin pan. Bake about 18-20 minutes. Let muffins cool before removing from pan.']
+                    'steps': 'Preheat oven to 400 degrees. Prepare a muffin pan. In a large bowl, sift together flour, sugar, baking powder, and salt. In a medium bowl, whisk in eggs. Add butter, milk and vanilla. Add to flour mixture and stir until combined. Fold in blueberries. Add mix to muffin pan. Bake about 18-20 minutes. Let muffins cool before removing from pan.'
                 }
         return recipe
     elif image.filename == 'poppyseed.jpeg':
         recipe = {
                     'title': 'Poppy Seed Chicken Casserole',
                     'ingredients': ['4 boneless, skinless chicken breasts, boiled and shredded in large pieces', '1/2 medium yellow onion, diced', '1 (10.75oz) can cream of chicken', '16oz sour cream', '8oz cream cheese, softened', '1 tablespoon poppy seeds', '1/2 teaspoon salt', '1/4 teaspoon black pepper', '1/2 cup melted butter', '1.5 sleeves Ritz crackers, crushed'],
-                    'steps': ['Preheat over to 350 degrees. In a large bowl, mix together cream of chicken, sour cream, cream cheese, poppy seeds, salt and pepper. Add onion and chicken. Transfer mixture to a casserole dish. Top evenly with crushed crackers and drizzle melted butter on top. Bake for 25 minutes.']
+                    'steps': 'Preheat over to 350 degrees. In a large bowl, mix together cream of chicken, sour cream, cream cheese, poppy seeds, salt and pepper. Add onion and chicken. Transfer mixture to a casserole dish. Top evenly with crushed crackers and drizzle melted butter on top. Bake for 25 minutes.'
                  }
         return recipe
 
